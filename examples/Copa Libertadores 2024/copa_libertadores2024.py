bb7a10e72efe418751ec7fcb1577e4182e1ed8a6
Kevin Bourdon <kevomhd@gmail.com>
2024-04-22 22:14:33 +0200

    feat: initialization library

---
+++ examples/Copa Libertadores 2024/copa_libertadores2024.py
@@ -0,0 +1,3 @@
+import distance_reducer as dr
+
+dr.solve_problem("../data/copa-libertadores-stadiums-2024.csv", 8, ['Pot'], ['Country'], plot=True, save=False, kpi=True)
---
+++ examples/French Ligue 1 2022-2023/french_ligue1_2022-23.py
@@ -0,0 +1,3 @@
+import distance_reducer as dr
+
+dr.solve_problem("../data/french_ligue1_2022.csv", 4, list_var_equal_to_one=[], list_var_maximum_one=[], plot=True, save=False, kpi=True)
